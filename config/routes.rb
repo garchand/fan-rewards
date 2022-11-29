d88eb3e86b551707e728101ac6b2b8ccacca6a91
Mario Aouane <marioaouane@gmail.com>
2022-11-29 14:33:04 +0100

    ajout du controller campaign

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :restaurant, only: %i[index new create show]
+  resources :restaurants, only: %i[index new create show] do
+    resources :campaigns, only: %i[index new create show destroy]
+  end
 end
