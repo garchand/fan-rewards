026f44a92a69d1a47a90647ae8a0d7b1a7c3f2f6
Gauthier Marchand <gauthier.marchand@centrale-marseille.fr>
2022-12-02 10:38:33 +0100

    Ajout de la seed

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module FanRewards
   class Application < Rails::Application
     config.generators do |generate|
@@ -23,5 +24,7 @@ module FanRewards
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+
   end
 end
