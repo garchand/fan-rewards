7940bbb582c597f884542b7bc375ca780a3c2c5b
Julien Nguyen <nguyenjulien93@gmail.com>
2022-12-05 09:28:33 +0100

    chatroom fonctionnel mais pas de  mise en page

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   end
   resources :restaurants, only: %i[index new create show] do
     resources :campaigns, only: %i[new create]
+    resources :chatrooms, only: :index
   end
   resources :campaigns, only: %i[show update] do
     resources :campaigns_ambassadors, only: %i[create]
   end
   get "stats", to: "pages#stats"
+
+resources :chatrooms, only: :show do
+  resources :messages, only: :create
+end
 end
