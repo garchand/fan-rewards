400fa8bf682b03d83c6cc7a54f4ccc21f62cf75b
Gauthier Marchand <gauthier.marchand@centrale-marseille.fr>
2022-12-05 11:14:13 +0100

    Résolution de problèmes sur la chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,4 +4,12 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find_by(restaurant: Restaurant.find(params[:restaurant_id]))
     @message = Message.new
   end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(restaurant_id: 5, name: "AA")
+  end
 end
