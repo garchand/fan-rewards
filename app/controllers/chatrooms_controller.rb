41ec5c085c38de651f992c7c77593666c0313402
Alban de La Rochefordiere <dlralban@gmail.com>
2022-12-06 15:43:04 +0100

    links on navbar

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,11 +3,12 @@ class ChatroomsController < ApplicationController
     policy_scope(Chatroom)
     @chatroom = Chatroom.find_by!(restaurant: Restaurant.find(params[:restaurant_id]))
     @message = Message.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def new
     @chatroom = Chatroom.new
   end
 
- 
+
 end
