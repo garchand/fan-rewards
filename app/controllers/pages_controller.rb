a6f9f2a9f04b3ac6558acd8483401e0ad2948c1d
Gauthier Marchand <gauthier.marchand@centrale-marseille.fr>
2022-12-07 10:09:10 +0100

    test-front-alban

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,8 @@ class PagesController < ApplicationController
     @campaigns_ambassadors = CampaignsAmbassador.all
     @current_restaurant = Restaurant.find(restaurants_params)
 
-    @number_of_ambassadors = @restaurants_ambassadors.where(restaurant_id: @current_restaurant).count
+    @restaurant_current_ambassadors = @restaurants_ambassadors.where(restaurant_id: @current_restaurant)
+    @number_of_ambassadors = @restaurant_current_ambassadors.count
 
     date0 = Date.today-30
     @number_of_new_ambassadors_over_the_last_30_days = @restaurants_ambassadors.where("created_at >= :date OR updated_at >= :date", date: date0).count
@@ -34,7 +35,16 @@ class PagesController < ApplicationController
 
     @number_of_rewards_used = CampaignsAmbassador.where(campaigns: @campaigns_of_the_restaurant).where(reward_status: "used").count
 
+    @cumulated_number_of_ambassadors = @restaurant_current_ambassadors.group_by_month(:created_at).count
 
+    @result_cumulated_number_of_ambassadors = {}
+    keys = []
+    @cumulated_number_of_ambassadors.each_with_index do |(key, value), index|
+      keys << key
+      @result_cumulated_number_of_ambassadors[key] = @cumulated_number_of_ambassadors.values_at(*keys).sum
+    end
+
+    raise
 
   end
 
